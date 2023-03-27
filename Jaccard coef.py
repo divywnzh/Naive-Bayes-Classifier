730bcfc3510f66b8d37bf3135c9804032180e9a2
Muskan190502 <muskan20087@iiitd.ac.in>
2023-03-27 23:38:12 +0530

    correct

--- Jaccard coef.py
+++ Jaccard coef.py
@@ -5,27 +5,39 @@ import math
 from q1b import partB #pre processing
 
 
-# Create sets of tokens for query and documents
+# Create sets of terms
+# for query and documents
 query=input("What are you looking for?: ")
 query=partB(query)
-query_tokens=set(list(map(str,query.split(" "))))
-with open('unigram_index.pickle', 'rb') as f:
-    unigram_matrix = pickle.load(f)
-document_tokens = [set(unigram_matrix.keys())]
+query_terms=set(list(map(str,query.split(" "))))
+print(query_terms)
 
-# Calculate Jaccard coefficient for each document
-jaccard_values = []
-for i in range(1400):
-    intersection = len(query_tokens.intersection(document_tokens[i]))
-    union = len(query_tokens.union(document_tokens[i]))
-    jaccard = intersection / union
-    jaccard_values.append((i, jaccard))
+docs_dict = {}
+for i in range(1, 1401):
+    file_name = f"CSE508_Winter2023_Dataset/cranfield{str(i).zfill(4)}"
+    with open(file_name, "r") as file:
+        data = file.read()
+        data = set(list(map(str,data.split(" "))))
+    docs_dict[i]=data
+
+jaccard_coeff_values = []
+for i in range(1,1401):
+    intersection_set= query_terms.intersection(docs_dict[i])
+    intersection = len(intersection_set)
+    union_set = query_terms.union(docs_dict[i])
+    union = len(union_set)
+    if union == 0:
+        jaccard=0
+    else:
+        jaccard = intersection / union
+    jaccard_coeff_values.append((i, jaccard))
 
 # Rank documents based on Jaccard coefficient value
-top_10_documents = sorted(jaccard_values, key=lambda x: x[1], reverse=True)[0:10]
+documents_descending_order = sorted(jaccard_coeff_values, reverse=True)
+top_10 = documents_descending_order[0:10]
+
 
 # Print top 10 documents
-for i, jaccard in top_10_documents:
-    print(f"Document {i+1}: Jaccard coefficient = {jaccard:.4f}")
-    # print(documents[i])
+for i, jaccard in top_10:
+    print("Document {}: Jaccard coefficient = {:.4f}".format(i+1, jaccard))
     print()
